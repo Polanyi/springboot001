05b82bf649c718e267f0a5dac19e43972ca99187
Ikseong Bae <ikseong.bae@gmail.com>
2019-04-24 17:21:28 +0900

    first commit

---
+++ z_SpringBootSample02/src/main/java/com/isb/demo/ZSpringBootSample02Application.java
@@ -0,0 +1,13 @@
+package com.isb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZSpringBootSample02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZSpringBootSample02Application.class, args);
+	}
+
+}
